1272bbb696ad1de43f71c36dca5eb26b8ecc4cd4
wixboops <coke3@dr.com>
2025-06-17 12:42:36 -0500

    Update lumin.lua

--- lumin.lua
+++ lumin.lua
@@ -1,6 +1,10 @@
-spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
-end)
+if identifyexecutor and identifyexecutor():lower():find("delta") then
+    print("delta")
+else
+    spawn(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
+    end)
+end
 
 
 if identifyexecutor and identifyexecutor():lower():find("delta") then
